75449986be16d88387902eb91a951be599d36d1c
NitendraHCL <NitendraAgarwal@hclhealthcare.in>
2025-11-04 21:23:33 -0800

    Update server.js

--- models/server.js
+++ models/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import spinboardRoutes from "./routes/spinboardRoutes.js";
+import spinboardRoutes from "../routes/spinboardRoutes.js";
 
 dotenv.config();
 const app = express();
